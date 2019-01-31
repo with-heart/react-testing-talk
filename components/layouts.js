fb565c61bb42b2086febc2772b67f92f5bc3863c
Mark Chandler <mark@chanchan.io>
2019-01-31 17:09:27 -0500

    chore: Create layout components

--- components/layouts.js
+++ components/layouts.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Children} from 'react'
 import {Split} from 'mdx-deck/layouts'
 
 export const CodeLayout = ({children}) => (
@@ -15,3 +15,42 @@ export const CodeLayout = ({children}) => (
     {children}
   </div>
 )
+
+export const SplitCodeLayout = ({children}) => {
+  const [a, ...rest] = Children.toArray(children.props.children)
+
+  return (
+    <div
+      style={{
+        backgroundColor: '#282a36',
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div style={{width: '50%'}}>{a}</div>
+      <div style={{width: '50%'}}>{rest}</div>
+    </div>
+  )
+}
+
+export const HorizontalSplit = ({children}) => {
+  const [a, ...rest] = Children.toArray(children.props.children)
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        height: '100vh',
+      }}
+    >
+      <div>{a}</div>
+      <div>{rest}</div>
+    </div>
+  )
+}
